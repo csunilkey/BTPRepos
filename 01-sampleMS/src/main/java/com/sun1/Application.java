81d61c6e170b27aafaf8a46636bf0273a89c6aff
csunilkey <csunil@SCHENNAMT480S.keyfoods.com>
2022-08-21 15:40:31 -0400

    GIT test - Comment Added

--- 01-sampleMS/src/main/java/com/sun1/Application.java
+++ 01-sampleMS/src/main/java/com/sun1/Application.java
@@ -3,7 +3,7 @@ package com.sun1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+//Below is GIT test
 
 @SpringBootApplication
 public class Application {
