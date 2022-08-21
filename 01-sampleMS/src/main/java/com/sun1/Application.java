8f2c7115862d29e3eb5cc9a86ecca2ef246ed472
csunilkey <csunil@SCHENNAMT480S.keyfoods.com>
2022-08-21 15:39:43 -0400

    Local - 03

--- 01-sampleMS/src/main/java/com/sun1/Application.java
+++ 01-sampleMS/src/main/java/com/sun1/Application.java
@@ -3,6 +3,8 @@ package com.sun1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class Application {
 
